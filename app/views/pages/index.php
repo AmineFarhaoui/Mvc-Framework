87ebde191c79c483eb67f5bd6fb799520f702538
AStackk <84063@roc-teraa.nl>
2021-05-23 20:12:12 +0200

    Update index.php
    Updated index.php with little information

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,3 +1,5 @@
 <?php require APPROOT . '/views/inc/header.php';?>
     <h1><?php echo $data['title'] ?></h1>
+    <h2>Mvc-Framework<br>
+    Please check the ReadMe.txt before beginning</h2>
 <?php require APPROOT . '/views/inc/footer.php';?>
