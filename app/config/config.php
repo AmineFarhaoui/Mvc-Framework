6936658872a421651b36d4d7e54b541139726ce8
AStackk <84063@roc-teraa.nl>
2021-05-23 17:56:41 +0200

    Added all functionallity
    The framework is ready to be used. Whole mvc concept with pdo finished!

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+    // DB Params *DON'T FORGET TO CHANGE YOUR DATA*
+    define('DB_HOST', 'localhost');
+    define('DB_USER', '_YOUR_USERNAME_');
+    define('DB_PASS', '_YOUR_PASSWORD_');
+    define('DB_NAME', '_YOUR_NAME_');
+
+    // App Root
+    define('APPROOT', dirname(dirname(__FILE__)));
+    
+    // URL Root *Example: http://localhost/Mvc-Framework/*
+    define('URLROOT', '_YOUR_URL_');
+
+    // Site Name
+    define('SITENAME', '_YOUR_SITENAME_');
